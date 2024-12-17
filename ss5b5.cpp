2577936af592667bf68ec1857b4e0d005313f5aa
pehaw06zz <nhattran22447@gmail.com>
2024-12-17 17:56:06 +0700

    Add files via upload

---
+++ ss5b5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	for(int i=1;i<=9;i++){
+		printf("bang cuu chuong tu 1-9 la: ");
+		for(int j=1;j<=9;j++){
+		printf("%d x %d =%d.\n",i,j,i*j);
+	}
+	printf("\n");
+  
+  
+}
+return 0;
+}
